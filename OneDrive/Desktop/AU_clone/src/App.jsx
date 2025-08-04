7b8e021ed63b6dce717399892cf8bc0726ba65eb
ShraddhaGhadi <shraddhaghadi311@gmail.com>
2025-08-04 11:30:08 +0530

    Updated project files

---
+++ OneDrive/Desktop/AU_clone/src/App.jsx
@@ -0,0 +1,69 @@
+import { FormProvider, useForm } from 'react-hook-form'
+import './App.css'
+import Otp from './components/Otp'
+import Phone from './components/Phone'
+import * as z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod'
+import {useState } from 'react'
+
+
+function App() {
+
+  const [step, setStep] = useState(1);
+  // console.log(step)
+
+  const phoneSchema = z.object({
+    phoneNumber: z.string().regex(/^\d{10}$/, 'Enter a valid 10-digit phone number').transform((val) => Number(val)),
+  })
+  const otpSchema = z.object({
+    otp: z.string().regex(/^\d{6}$/, 'OTP must be a 6-digit number').transform((val) => Number(val)),
+  })
+
+
+  const getSchema = () => {
+    if (step === 1) return phoneSchema;
+    if (step === 2) return otpSchema;
+    return otpSchema;
+  }
+
+
+  const methods = useForm({
+    mode: 'onChange',
+    resolver: zodResolver(getSchema()),
+    defaultValues: {
+      phoneNumber: '',
+      otp: '',
+      name: '',
+      dob: '',
+      address: ''
+    }
+  });
+  const { } = methods;
+
+  return (
+    <FormProvider {...methods}>
+      <div className='bg-fuchsia-800 h-screen p-8 flex justify-center items-center' >
+        <div className='p-6 bg-white w-[400px] py-4 rounded-xl shadow'>
+          <h2 className='text-2xl font-semibold text-center mb-2'>Sign Up</h2>
+          {step === 1 &&
+            <Phone setStep={setStep} />
+          }
+          {step === 2 &&
+            <Otp setStep={setStep} />
+          }
+
+           {step === 0 &&
+           (<>
+           <h1 className='text-center text-xl font-semibold'>Signed in Successfully</h1>
+           </>)
+          }
+
+
+
+        </div>
+      </div>
+    </FormProvider>
+  )
+}
+
+export default App
